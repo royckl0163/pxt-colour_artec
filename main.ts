b6956aa2bfc9c247fc61fe8a3cb2bb3d6aaf8763
royckl0163 <60773061+royckl0163@users.noreply.github.com>
2020-02-15 17:25:06 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -5,3 +5,12 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `);
+
+//% color=#5b99a5 weight=100 icon="\uf009" block="Colour_ArtecRobo"
+namespace colour {
+
+    //% blockId=read_colour_value
+    
+
+
+}
