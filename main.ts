5e838c6628e904f6c092453de790255c88517021
royckl0163 <60773061+royckl0163@users.noreply.github.com>
2020-02-18 23:09:50 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ enum RGB{
 
   //% color=#5b99a5 weight=100 icon="\uf009" block="colour_artec"
   namespace colour {
-
+      
     //% blockId=getRGB block="get colour value %_colourVal"
     export function getRGB(_colour: RGB): number{
         
